9f84685969c0476a706bf84a3e908e26c018f700
luffy <luffy@gmail.com>
2024-03-12 01:53:31 +0530

    3nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
           speed={2}
           onClick={() => ref.current.scrollTo(0)}
         >
-          <h2>Hi Mom!</h2>
+          <h2>MEOW!!</h2>
         </ParallaxLayer>
       </Parallax>
     </div>
